50df583176fc01e0ecbb0478c33318c14b4b30b4
mbardauskas <martynas.bardauskas@gmail.com>
2013-08-28 17:15:20 +0300

    terminate worker threads
    Each worker thread should be terminated after it finishes processing the image, otherwise the browser keeps it alive and eventually the script hits the maximum concurrent worker limit.

--- src/BnWWorker.js
+++ src/BnWWorker.js
@@ -12,4 +12,16 @@ this.onmessage = function (event) {
 	}  
 	
 	postMessage(imagedata);
+	
+	// terminate the worker
+	
+	// the standard way - http://www.w3.org/TR/workers/#dedicated-workers-and-the-worker-interface
+	if (this.terminate) {
+	    this.terminate();
+	}
+	
+	// IE 10 specific - http://msdn.microsoft.com/en-us/library/ie/hh673568(v=vs.85).aspx
+	if(this.close) {
+	    this.close();
+	}
 }
